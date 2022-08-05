154177e8d44ef7ac7d6002ca43a87a558faaa1e9
Gustavo Silv4 <contato@gustavosilv4.com.br>
2022-08-05 16:53:23 -0300

    feat: created structure and functions of modal functionalities

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,78 @@
+import { useState } from 'react'
+
 import { Card } from '../../components/Card'
-import { CardSection, Container, Header } from './styles'
+import { Table } from './components/Table'
+import { ModalCategory } from './components/ModalCategory'
+import { ModalTransaction } from './components/ModalTransaction'
 
 import logo from '../../assets/finance-image.png'
-import { Table } from '../../components/Table'
+import { CardSection, Container, Header } from './styles'
+
+export interface TransactionListData {
+  id: number
+  title: string
+  value: string
+  type: string
+  category: string
+  date: string
+}
+
+export interface CategoriesData {
+  id: number
+  name: string
+}
 
 export function Home() {
+  const [showModalCategory, setShowModalCategory] = useState(false)
+  const [showModalTransaction, setShowModalTransaction] = useState(false)
+
+  const [transactionList, setTransactionList] = useState<TransactionListData[]>([])
+
+  const [category, setCategory] = useState<CategoriesData[]>([
+    { id: 1, name: 'Venda' },
+    { id: 2, name: 'Alimentação' },
+    { id: 3, name: 'Casa' },
+  ])
+
+  function handleShowModalCategory() {
+    setShowModalCategory(!showModalCategory)
+  }
+
+  function handleShowModalTransaction() {
+    setShowModalTransaction(!showModalTransaction)
+  }
+
+  function createNewCategory(category: string) {
+    const newCategory = {
+      id: new Date().getTime(),
+      name: category,
+    }
+
+    setCategory((state) => [...state, newCategory])
+  }
+
+  function createNewTransaction(data: TransactionListData) {
+    const newTransaction = {
+      id: data.id,
+      title: data.title,
+      value: data.value,
+      type: data.type,
+      category: data.category,
+      date: data.date,
+    }
+
+    setTransactionList((state) => [...state, newTransaction])
+  }
+
+  console.log(transactionList)
+
   return (
     <Container>
       <Header>
         <img src={logo} alt="" />
         <nav>
-          <button>Criar categoria</button>
+          <button onClick={handleShowModalCategory}>Criar categoria</button>
+          <button onClick={handleShowModalTransaction}>Transação</button>
         </nav>
       </Header>
       <main>
@@ -21,6 +83,17 @@ export function Home() {
         </CardSection>
 
         <Table />
+        <ModalCategory
+          show={showModalCategory}
+          closeModal={handleShowModalCategory}
+          createCategory={createNewCategory}
+        />
+        <ModalTransaction
+          show={showModalTransaction}
+          closeModal={handleShowModalTransaction}
+          categories={category}
+          createTransaction={createNewTransaction}
+        />
       </main>
     </Container>
   )
